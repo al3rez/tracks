1d01fd03097dcacc1313fc9572cde37ecc0fe977
Alireza Bashiri <al3rez@gmail.com>
2025-08-09 11:54:35 +0700

    Remove sample application code from framework
    - Remove Post and Comment models
    - Remove PostsController and CommentsController
    - Remove posts views directory
    - Keep framework clean without sample app code

--- app/Views/posts/edit.php
+++
@@ -1,6 +0,0 @@
-<h1>Edit Post</h1>
-
-<?= $renderPartial('posts/form', ['post' => $post]) ?>
-
-<?= $linkTo('Show', "/posts/{$post->id}") ?> |
-<?= $linkTo('Back', '/posts') ?>
--- app/Views/posts/new.php
+++
@@ -1,5 +0,0 @@
-<h1>New Post</h1>
-
-<?= $renderPartial('posts/form', ['post' => $post]) ?>
-
-<?= $linkTo('Back', '/posts') ?>
